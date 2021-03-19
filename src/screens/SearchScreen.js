5eae742dee84985631d9bed2c7893c39b2f754f9
Fisayo <48415629+fisayowatti@users.noreply.github.com>
2021-03-19 09:10:15 +0100

    Create the different screens

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function SearchScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>This is the search screen</Text>
+    </View>
+  );
+}
